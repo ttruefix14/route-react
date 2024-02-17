c9b4b9f34197418b34926cf5f2a60689082e7b73
ttruefix14 <ttruefix14@gmail.com>
2024-02-17 19:19:49 +0300

    useSelector

--- src/components/pages/SomeList/MemberPage.tsx
+++ src/components/pages/SomeList/MemberPage.tsx
@@ -2,15 +2,17 @@ import { useParams } from "react-router";
 
 import { ReactNode } from "react";
 import Member from "../../../models/Member";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 
 interface MemberProps {
     selectedMember?: Member;
 }
 
 function MemberPage(props: MemberProps) {
+    const counter = useSelector((x: any) => x.counter)
     return <>
         <p>User: {props.selectedMember?.username} with password: {props.selectedMember?.password}</p>
+        <p>{JSON.stringify(counter)}</p>
     </>
 }
 
